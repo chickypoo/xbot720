b90a5c5275186896e52c65429e323e6c70bbfcad
chickypoo <tankzu1016@gmail.com>
2024-05-01 00:02:10 -0700

    Finalized Code
    Added program for the 2 remote vehicles.
    Both program are the same one, only using different motor therefore different count per 1 shaft revolution.
    The admin code is inoperative at the moment

---
+++ remoteCar_PCM_car2/remoteCar_PCM_car2.ino
@@ -0,0 +1,435 @@
+/***************************************************************************
+ *                            Bitwise Definition                           *
+ ***************************************************************************/
+#define BIT0    0x01
+#define BIT1    0x02
+#define BIT2    0x04
+#define BIT3    0x08
+#define BIT4    0x10
+#define BIT5    0x20
+#define BIT6    0x40
+#define BIT7    0x80
+/***************************************************************************
+ *                        Arduino Pinout Definition                        *
+ ***************************************************************************/
+#define TX      1   // HC-05 RX
+#define RX      0   // HC-05 TX
+
+#define RCHA    2   // Right Encoder Channel A
+#define LCHA    3   // Left Encoder Channel A
+#define RIN1    6   // Right Motor Enable 1
+#define RIN2    5   // Right Motor Enable 2
+#define LIN1    9   // Left Motor Enable 1
+#define LIN2    10  // Left Motor Enable 2
+/***************************************************************************
+ *                           System Specification                          *
+ ***************************************************************************/
+#define WHEEL_DIA_MM 90
+#define PI           3.1415
+#define WHEEL_TRAVEL WHEEL_DIA_MM * PI
+#define CPR          757       // Count per Revolution
+
+#define SPEED_VARIANCE 5
+#define DEFAULT_MIN_SPEED 100  // in mm/s
+#define DEFAULT_MAX_SPEED 500  // in mm/s
+#define MAX_SPEED         3650 // in mm/s
+/***************************************************************************
+ *                            Static Receive Code                          *
+ ***************************************************************************/
+#define LDIR    BIT7           // Left Direction Bit
+#define LS0     BIT6
+#define LS1     BIT5
+#define LS2     BIT4
+#define RDIR    BIT3           // Right Direction Bit
+#define RS0     BIT2
+#define RS1     BIT1
+#define RS2     BIT0
+
+#define LSPD    (LS0 | LS1 | LS2)
+#define RSPD    (RS0 | RS1 | RS2)
+
+#define FASTEST BIT2
+#define FAST    (BIT2 | BIT1)
+#define NORMAL  BIT1
+#define SLOW    (BIT1 | BIT0)
+#define SLOWEST BIT0
+#define STOP    0
+#define ERRSPD  119
+
+#define AD_CD1  63              // Special Admin Code to SET speed
+#define AD_CD2  127             // Special Admin Code to ECHO speed
+/***************************************************************************
+ *                            Extra Specifications                         *
+ ***************************************************************************/
+#define LEFT    0
+#define RIGHT   1
+#define FWD     0
+#define RWD     1
+#define BRK     2
+
+#define AVG_LEN 4         // Averager length
+#define SEQ     0         // Toggle this to run sequence testing
+/***************************************************************************
+ *                           Status Bit Definition                         *
+ ***************************************************************************/
+#define PCM     BIT0      // Calculate the Current Speed
+#define CALC    BIT1      // Calculate the Target Speed
+#define READ    BIT2      // Receive latest command from bluetooth
+/***************************************************************************
+ *                                 Globals                                 *
+ ***************************************************************************/
+volatile unsigned int counts[2] = {0};   // Real time counter for Left Channel A
+const float KP = 0.61;                         // Proportion Control
+const float KI = 2.73;                         // Integral Control
+const float KD = 0.08;                         // Derivative Control
+
+float speedTotalError[2] = {0};                 // eIntegral for PID
+float pSpeedError[2] = {0};                     // Previous Error for the Derivative Control
+
+unsigned char code = 0;                  // Incoming code from Vision Controller
+
+unsigned int arrCounts[2][AVG_LEN] = {0};       // Ongoing Averager for the count
+size_t indexer = 0;                             // Iterator for the averager
+
+float speedY[2] = {0};                          // Current Speed (from reading)
+float speedR[2] = {0};                          // Speed Target
+int power[2] = {0};                   // power needed to drive for speed
+unsigned char motorDir[2] = {0};                // Direction of motors
+
+unsigned char status = 0;                       // Main loop status
+unsigned int speed[SPEED_VARIANCE];             // This is the range of speed the car uses
+
+/***************************************************************************
+ *                               Test Run Values                           *
+ ***************************************************************************/
+// Test sequence for increasing speed in 1 direction
+unsigned char testSequence1[] = {0x10, 0x11, 0x31, 0x33, 0x23, 0x22, 0x62, 0x66, 0x46, 0x44, 0x00};
+// Test sequence for direction switching
+unsigned char testSequence2[] = {0x22, 0xAA, 0x22, 0xAA, 0x00, 0x2A, 0xA2, 0x2A, 0xA2, 0x00, 0x00};
+// Test sequence for going to rest
+unsigned char testSequence3[] = {0x11, 0x00, 0x22, 0x00, 0x44, 0x00, 0xCC, 0x00, 0x44, 0x00, 0x00};
+// Test sequence for 1 sided scale
+unsigned char testSequence4[] = {0x01, 0x03, 0x02, 0x06, 0x04, 0x06, 0x02, 0x03, 0x01, 0x00, 0x00};
+size_t indexSequence = 0;
+bool runSEQ = SEQ;
+
+void setup() {
+  Serial.begin(57600);
+  // Initiate the speed array
+  setSpeed(DEFAULT_MIN_SPEED, DEFAULT_MAX_SPEED, SPEED_VARIANCE);
+  // Setup the pinout
+  pinMode(RIN1, OUTPUT);
+  pinMode(RIN2, OUTPUT);
+  pinMode(RCHA, INPUT);
+  attachInterrupt(digitalPinToInterrupt(RCHA), rPulse, RISING);
+
+  pinMode(LIN1, OUTPUT);
+  pinMode(LIN2, OUTPUT);
+  pinMode(LCHA, INPUT);
+  attachInterrupt(digitalPinToInterrupt(LCHA), lPulse, RISING);
+  // Setup a timers
+  cli();
+  // Using Timer 2 because pin 11 and 13 is free
+  TCCR2A = TCCR2B = 0;            // Clear register for Timer 2
+
+  TCCR2B |= (BIT2 | BIT1 | BIT0); // Prescalar 1:1024
+  TIMSK2 |= BIT1;                 // CC Timer 2A
+  OCR2A = 125;                    // Timer 2 interrupt every 8 ms
+  sei();
+}
+
+void loop() {
+  // Using pulse counting method to check amount of counts in each main loop
+  static unsigned long pt;
+  float currentCounts[2], dt;
+  unsigned long t;
+
+  if(status & PCM){
+    // Turn off Interrupts to capture pulse counts
+    noInterrupts();                             // Turn off interrupt to encoders
+    arrCounts[LEFT][indexer] = counts[LEFT];    // Record the volatile counts
+    arrCounts[RIGHT][indexer] = counts[RIGHT]; 
+    counts[LEFT] = counts[RIGHT] = 0;           // Zero the counts
+    indexer = (indexer + 1) % AVG_LEN;          // Increase the average index
+    interrupts();                               // Turn interrupt back on
+    // Averager through the pulse counter
+    getAverageCounts(currentCounts);
+    // Find the time difference delta T  
+    t = millis();
+    dt = (float)(t - pt) / 1000.0;
+    pt = t;
+    // Using the delta T and count difference, together with CPR and wheel size
+    // Find the speed the wheel travels at
+    calculateCurrentSpeed(currentCounts, dt);
+    calculateNewPower(LEFT, dt);
+    calculateNewPower(RIGHT, dt);
+    setMotorPower();
+
+    status &= ~PCM;
+  } else if(status & CALC){
+    // Use the code to decide the direction, and speed of each wheel
+    updateTarget();
+    updateDirection();
+    status &= ~CALC;
+  } else if(status & READ){   // Fetch code
+    if(Serial.available()){
+      code = Serial.read();
+      
+    }
+    status |= CALC;
+    status &= ~READ;
+  }  
+}
+
+/* setMotorPower
+ * This sets the motors from the calculated value in global
+ */
+void setMotorPower(){
+  setMotor(LEFT, motorDir[LEFT], power[LEFT]);
+  setMotor(RIGHT, motorDir[RIGHT], power[RIGHT]);
+}
+
+/* calculateNewPower
+ * This function calculates what the power should be at based on current speed and target speed
+ * argument: side, LEFT or RIGHT side of the vehicle
+ * argument: dt, time since last time the function is called in the main loop
+ */
+void calculateNewPower(unsigned char side, float dt){
+  float speedE, speedD, speedU;
+
+  speedE = speedR[side] - speedY[side];                       // Calculate E(s)
+  speedTotalError[side] += speedE * dt;                       // Calculate eIntegral(s)
+  speedD = (speedE - pSpeedError[side]) / dt;  // Calculate E'(s)
+
+  speedU = KP * speedE + KI * speedTotalError[side] + KD * speedD; // Calculate U(s)
+
+  pSpeedError[side] = speedE;                                 // Update deltaE
+  if(speedY[side] == speedR[side])
+    speedTotalError[side] = 0;
+
+  power[side] = (int)(speedU / unitPVS(speedU));
+  if(power[side] > 255)
+    power[side] = 255;
+  else if(power[side] < 0)
+    power[side] = 0;
+}
+
+/* getAverageCounts
+ * This function does the averager for the counts
+ * argument: avgCurrentCounts, an array of counts
+ */
+void getAverageCounts(float * avgCurrentCounts){
+  avgCurrentCounts[LEFT] = average(arrCounts[LEFT], AVG_LEN);
+  avgCurrentCounts[RIGHT] = average(arrCounts[RIGHT], AVG_LEN);
+}
+
+/* calculateCurrentSpeed
+ * This function calculate the speed of the wheels based on average counts
+ * argument: currentCounts, average of the count pair
+ * argument: dt, the time difference between each of the iteration
+ */
+void calculateCurrentSpeed(float * currentCounts, float dt){
+  speedY[LEFT] = currentCounts[LEFT] / CPR * WHEEL_TRAVEL / dt;
+  speedY[RIGHT] = currentCounts[RIGHT] / CPR * WHEEL_TRAVEL / dt;
+}
+
+/* updateTarget
+ * This function changes the speed target of both wheel
+ */
+void updateTarget(){
+  size_t index[2];
+  static unsigned char pSpeedCode = 0;
+  if(pSpeedCode == (code & (LSPD | RSPD)))
+    return;
+  // Byte shift to determine the speed code
+  // Extract the index of the speed code
+  index[LEFT] = speedIndexer((code & LSPD) >> 4);
+  index[RIGHT] = speedIndexer((code & RSPD));
+  // Set the Target Speed R(s)
+  if(index[LEFT] != ERRSPD)
+    speedR[LEFT] = (index[LEFT] == SPEED_VARIANCE) ? 0 : speed[index[LEFT]];
+  if(index[RIGHT] != ERRSPD)
+    speedR[RIGHT] = (index[RIGHT] == SPEED_VARIANCE) ? 0 : speed[index[RIGHT]];
+
+  pSpeedCode = (code & (LSPD | RSPD));
+}
+
+/* updateDirection
+ * This function changes the direction of both wheel
+ */
+void updateDirection(){
+  static unsigned char pDirectionCode = 0;
+  if(pDirectionCode == (code & (LDIR | RDIR)))
+    return;
+  // Byte shift to determine the direction code
+  // Extract the direction of the directional code
+  motorDir[LEFT] = ((code & LDIR) >> 7);
+  motorDir[RIGHT] = ((code & RDIR) >> 3);
+
+  pDirectionCode = (code & (LDIR | RDIR));
+}
+
+/* speedIndexer
+ * This returns the index of the corresponding speed from the code
+ * argument: speedCode, the 3-bit speed code
+ * return: the index of the speed
+ */
+size_t speedIndexer(unsigned char speedCode){
+  switch(speedCode){
+    case FASTEST: return 4;
+    case FAST:    return 3;
+    case NORMAL:  return 2;
+    case SLOW:    return 1;
+    case SLOWEST: return 0;
+    case STOP:    return SPEED_VARIANCE;
+    default:      return ERRSPD;
+  }
+}
+
+/* setMotor
+ * This is the manual way to set the motor speed by calling the setRightMotor and setLeftMotor
+ * argument: side, the left or right wheel selection
+ * argument: dir, the direction to rotate the wheel in order to achieve motion
+ * argument: pwm, the speed value scaling from 0-255 of the 6V motor
+ */
+void setMotor(unsigned char side, unsigned char dir, unsigned char pwm){
+  switch(side){
+    case LEFT:
+      setLeftMotor(dir, pwm);
+      break;
+    case RIGHT:
+      setRightMotor(dir, pwm);
+      break;
+    default:
+      break;
+  }
+}
+
+/* setRightMotor
+ * This is the manual way to set the right motor speed
+ * argument: dir, the direction to rotate the wheel in order to achieve motion
+ * argument: pwm, the speed value scaling from 0-255 of the 6V motor
+ */
+void setRightMotor(unsigned char dir, unsigned char pwm){
+  switch(dir){
+    case FWD:
+      digitalWrite(RIN2, LOW);
+      analogWrite(RIN1, pwm);
+      break;
+    case RWD:
+      digitalWrite(RIN1, LOW);
+      analogWrite(RIN2, pwm);
+      break;
+    case BRK:
+      digitalWrite(RIN1, 0);
+      digitalWrite(RIN2, 0);
+      break;
+    default:
+      break;
+  }
+}
+
+/* setLeftMotor
+ * This is the manual way to set the left motor speed
+ * argument: dir, the direction to rotate the wheel in order to achieve motion
+ * argument: pwm, the speed value scaling from 0-255 of the 6V motor
+ */
+void setLeftMotor(unsigned char dir, unsigned char pwm){
+  switch(dir){
+    case FWD:
+      digitalWrite(LIN2, LOW);
+      analogWrite(LIN1, pwm);
+      break;
+    case RWD:
+      digitalWrite(LIN1, LOW);
+      analogWrite(LIN2, pwm);
+      break;
+    case BRK:
+      digitalWrite(LIN1, 0);
+      digitalWrite(LIN2, 0);
+      break;
+    default:
+      break;
+  }
+}
+
+/* setSpeed
+ * This function sets the speed of the car with new values
+ * argument: minSpeed, minimum speed of the car (in mm/s)
+ * argument: maxSpeed, maximum speed of the car (in mm/s)
+ * argument: amount, the different stages between the min and max speed
+ */
+void setSpeed(int minSpeed, int maxSpeed, int amount){
+  int deltaSpeed = maxSpeed - minSpeed;
+  int speedInc = deltaSpeed / (amount - 1);
+  for(int i = 0; i < amount; i++){
+    speed[i] = minSpeed + speedInc * i;
+  }
+}
+
+/* rPulse
+ * This function is for the interrupt for Right Encoder Channel A
+ * It only increment the count from right wheel by 1
+ */
+void rPulse(){
+  counts[RIGHT]++;
+}
+
+/* lPulse
+ * This function is for the interrupt for Left Encoder Channel A
+ * It only increment the count from left wheel by 1
+ */
+void lPulse(){
+  counts[LEFT]++;
+}
+
+// Timer 2A ISR (runs at 8ms);
+ISR(TIMER2_COMPA_vect){
+  static unsigned char i = 0, si = 0;
+  i++;
+  if(runSEQ)
+    si++;
+  
+  if(i == 10)
+    status |= PCM;    // Check Pulse every 8ms x 10 = 80ms of the 160ms
+  if(i == 20){
+    status |= READ;   // Check Serial import every 8ms x 10 = 80ms of the 160ms
+    i = 0;
+  }
+
+  if(si == 250){
+    code = testSequence4[indexSequence++];
+    if(indexSequence == 12){
+      runSEQ = false;
+    }
+    si = 0;
+  }
+  
+  TCNT2 = 0;
+}
+
+/* average
+ * This computes the average of a given array
+ * argument: a, the array of numerics to be averaged
+ * argument: n, the size of array a
+ */
+float average(unsigned int * a, size_t n){
+  unsigned int sum = 0;
+  for(size_t i = 0; i < n; i++)
+    sum += a[i];
+  return (float)sum / n;
+}
+
+/* unitPVS
+ * This function scales the given SPEED to a byte scale
+ * argument: s, the speed of the given motor
+ */
+float unitPVS(float s){
+  if(s > 1800) return 14.5;
+  if(s > 900) return 14.0;
+  if(s > 650) return 13.5;
+  if(s > 410) return 12.75;
+  if(s > 290) return 12.0;
+  if(s > 170) return 10.5;
+  return 8.5;
+}
